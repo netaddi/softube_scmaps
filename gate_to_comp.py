315e73d91cb0aeda62dd8e3a7e681c3e77e638e1
xi yang <xiyang@xis-Mac-mini.lan>
2023-08-24 00:13:50 +0800

    add

---
+++ gate_to_comp.py
@@ -0,0 +1,11 @@
+input = """
+MapStart<Alt2>
+PluginDisplayType: <Gate>
+ParamFuzz = <Control>(SHAPE_GateRelease)<Value>(0.5)
+ParamType = <Control>(SHAPE_HardGate)<Value>(0.0)
+ParamTone = <Control>(SHAPE_Sustain)<Value>(0.5)
+ParamOutput = <Control>(SHAPE_Gate)<Value>(0.5)<DisplayName>(Level)
+MapEnd
+"""
+
+
