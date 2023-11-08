b73eebeeede93a286750ef1b504dcfbc6adb2e3d
xi yang <xiyang@xis-Mac-mini.local>
2023-11-08 23:27:06 +0800

    change update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 set -x ;
 
-ls | grep ssx | xargs -I {} sudo cp -R {} "/Library/Application Support/Softube/SSX/";
+ls | grep ssx | xargs -I {} sudo cp -R {}/Contents/Resources/SCMap.json "/Library/Application Support/Softube/SSX/{}/Contents/Resources/SCMap.json";
 ps auxww  | grep "Console 1" | grep -v grep | awk '{print $2}' | xargs kill -9;
 open /Applications/Console\ 1\ On-Screen\ Display.app;
 
