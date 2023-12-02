5583231c609ec6fbb7511dda9496f094be4c17b0
wangyin <prof.weismann1@Gmail.com>
2023-12-02 23:07:48 +0800

    u

--- fetch.sh
+++ fetch.sh
@@ -2,5 +2,5 @@ set -x;
 
 export CURRENT_DIR=`pwd`;
 cd "/Library/Application Support/Softube/SSX/" && \
-find . -name 'SCMap.json' | xargs  -I {} rsync -R {} $CURRENT_DIR/;
+find . -name 'SCMap.json' | xargs  -I {} rsync -R {} $CURRENT_DIR/SSX/;
 
--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 set -x ;
 
-find . -name "*.json" | grep ssx | awk -F '/' '{print $2}' | xargs -I {} sudo cp -R ./{}/Contents/Resources/SCMap.json "/Library/Application Support/Softube/SSX/{}/Contents/Resources/SCMap.json";
-ps auxww  | grep "Console 1" | grep -v grep | awk '{print $2}' | xargs kill -9;
+ls ./SSX | xargs -I {} sudo cp -R ./SSX/{}/Contents/Resources/SCMap.json "/Library/Application Support/Softube/SSX/{}/Contents/Resources/SCMap.json";
+ps auxww  | grep "Console 1" | grep -v grep | awk '{print $2}' | xargs kill -9 && sleep 1 &&
 open /Applications/Console\ 1\ On-Screen\ Display.app;
 
