dc3c97295a59697a6618d809171f7cd7433ed232
wangyin <prof.weismann1@Gmail.com>
2024-03-17 17:49:43 +0800

    update compressors

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@ set -x ;
 
 ls ./SSX | xargs -I {} sudo cp -R ./SSX/{}/Contents/Resources/SCMap.json "/Library/Application Support/Softube/SSX/{}/Contents/Resources/SCMap.json";
 ps auxww  | grep "Console 1" | grep -v grep | awk '{print $2}' | xargs kill -9 && sleep 1 &&
+ps auxww  | grep "PluginDoctor" | grep -v grep | awk '{print $2}' | xargs kill -9 &&
 open /Applications/Console\ 1\ On-Screen\ Display.app;
+open /Applications/PluginDoctor.app;
 
