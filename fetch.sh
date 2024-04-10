2d0419a1af074224a66083d64b08b7144e5093c4
wangyin <prof.weismann1@Gmail.com>
2024-04-10 22:57:32 +0800

    add new

--- fetch.sh
+++ fetch.sh
@@ -3,4 +3,5 @@ set -x;
 export CURRENT_DIR=`pwd`;
 cd "/Library/Application Support/Softube/SSX/" && \
 find . -name 'SCMap.json' | xargs  -I {} rsync -R {} $CURRENT_DIR/SSX/;
+find . -name 'VST3SCMap*.json' | xargs  -I {} rsync -R {} $CURRENT_DIR/SSX/;
 
