fcfcb055b3048c2fdc4b5c7df563e7c2d7922d92
wangyin <prof.weismann1@Gmail.com>
2024-04-10 23:09:05 +0800

    u

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 set -x ;
 
-ls ./SSX | xargs -I {} sudo cp -R ./SSX/{}/Contents/Resources/SCMap.json "/Library/Application Support/Softube/SSX/{}/Contents/Resources/SCMap.json";
+ls ./SSX | grep -v VST3 | xargs -I {} sudo cp -R ./SSX/{}/Contents/Resources/SCMap.json "/Library/Application Support/Softube/SSX/{}/Contents/Resources/SCMap.json";
+ls ./SSX | grep VST3 | xargs -I {} sudo cp ./SSX/{} "/Library/Application Support/Softube/SSX/";
 ps auxww  | grep "Console 1" | grep -v grep | awk '{print $2}' | xargs kill -9 && sleep 1 &&
 ps auxww  | grep "PluginDoctor" | grep -v grep | awk '{print $2}' | xargs kill -9 &&
 open /Applications/Console\ 1\ On-Screen\ Display.app;
